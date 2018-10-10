aa2a1ed94bce2d80f3ad299bdf0371983c15dbea
skysec <hecber@gmail.com>
2018-10-09 23:00:09 -0400

    Added CIS 1.5 - 1.11
    Added Password Policies and moved MFA on root account to control
    1.13

--- controls/aws_aim_rules.rb
+++ controls/aws_aim_rules.rb
@@ -27,20 +27,37 @@ control 'aws-1.2' do
     end
 end
 
-control 'aws-1.2-a' do
+control 'aws-1.3 / aws-1.4' do
     impact 1.0
-    title 'Ensure MFA for all IAM users (root)'
-    desc "Ensure multi-factor authentication (MFA) is enabled for root user"
-    describe aws_iam_root_user do
-        it { should have_mfa_enabled }
-    end
-end
-
-control 'aws-1.3' do
-    impact 1.0
-    title 'Unused credentials'
+    title 'Unused credentials / Rotate Credentials'
     desc "Ensure credentials unused for 90 days or greater are disabled (Scored)"
     describe aws_iam_access_keys.where { created_days_ago > 90 } do
         it { should_not exist }
+    end
+end
+
+control 'aws-1.5 / aws-1.6 / aws-1.7 / aws-1.8 / aws-1.9 / aws-1.10 / aws-1.11' do
+    impact 1.0
+    title 'Password Complexity'
+    desc "Ensure IAM password policy requires at least: one uppercase letter,
+          one lowercase letter, one symbol, one number, minimum length of 14 or greater,
+          prevents password reuse, expires passwords within 90 days or less"
+    describe aws_iam_password_policy do
+        it { should require_uppercase_characters }
+        it { should require_lowercase_characters }
+        it { should require_symbols }
+        it { should require_numbers }
+        its('minimum_password_length') { should be > 13 }
+        it { should prevent_password_reuse }
+        its('max_password_age_in_days') { should be 90 }
+    end
+end
+
+control 'aws-1.13' do
+    impact 1.0
+    title 'Ensure MFA is enabled for the root account'
+    desc "Ensure  MFA is enabled for the root account (Scored)"
+    describe aws_iam_root_user do
+        it { should have_mfa_enabled }
     end
 end
